6b4a8bcac8d65be34596954e2599b82ddaea1bcb
NamHyunJu <72422188+NamHyunJu@users.noreply.github.com>
2020-12-08 09:20:21 +0900

    assets,index.html
    첫 업로드(12.08 am9:19)

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    //본문1 swiper
+    var swiper1 = new Swiper1('#cnt1 .swiper-container', {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        loop:true,
+        autoplay:{
+            delay:2000,
+        },
+        a11y: {
+            prevSlideMessage: '이전 슬라이드 보기',
+            nextSlideMessage: '다음 슬라이드 보기',
+            firstSlideMessage: '첫번째 슬라이드',
+            lastSlideMessage: '마지막 슬라이드',
+        },
+
+    });
+    //일시정지 클릭
+    $('#cnt1 .controller .stop').on('click', function () {
+        $(this).hide().siblings().show();
+        swiper1.autoplay.stop();
+        return false;//왜썻지?
+    });
+    //자동실행 클릭
+    $('#cnt1 .controller .play').on('click', function () {
+        $(this).hide().siblings().show();
+        swiper1.autoplay.start();
+        return false;
+    });
+
+    //본문2 swiper
+    var swiper2 = new Swiper2('#cnt2 .swiper-container', {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+    });
+
+});
